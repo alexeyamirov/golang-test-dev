64c3288d4dd3e2dc36eb59eb59a0cee222c0ac8a
Alexey <amirov.alex@gmail.com>
2026-02-28 21:00:00 +0300

    comments update

--- services/alert-processor/adapters/adapter.go
+++ services/alert-processor/adapters/adapter.go
@@ -11,8 +11,8 @@ type AlertResult struct {
 	Value int
 }
 
-// Adapter оценивает данные устройства и возвращает алерты при необходимости.
-// Adapter привязан к конкретной метрике/условию.
+// Adapter оценивает данные устройства и возвращает алерты при необходимости
+// Adapter привязан к конкретной метрике / условию
 type Adapter interface {
 	Evaluate(device *tr181.TR181Device) []AlertResult
 }
